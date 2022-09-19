71a6356306757dd64ac84627c5369d9c3e5a972b
Pracheth-SP <105972339+Pracheth-SP@users.noreply.github.com>
2022-09-19 20:58:25 +0530

    Add files via upload

---
+++ Lab-1 Assignment/src/com/greatlearning/main/DriverClass.java
@@ -0,0 +1,50 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+import com.greatlearning.model.*;
+import com.greatlearning.service.*;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee("Pracheth"," SP");
+		CredentialService cs = new CredentialService();
+		String generatedEmail;
+		char[] generatedPassword;
+		
+		System.out.println("Please enter the following Department");
+		System.out.println("1.Technical");
+		System.out.println("2.Admin");
+		System.out.println("3.Human Resource");
+		System.out.println("4.Legal");
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+		
+		if (option == 1) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(), employee.getLastName().toLowerCase(),"Technical");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		else if (option == 2) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(), employee.getLastName().toLowerCase(),"Admin");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		else if (option == 3) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(), employee.getLastName().toLowerCase(),"Human Resource");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		else if (option == 4) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(), employee.getLastName().toLowerCase(),"Legal");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		else
+			System.out.println("ENTER A VALID OPTION");
+		
+		sc.close();
+
+	}
+}
